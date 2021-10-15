b749780211638f4c8bf3ce5fcad3ed7bb1c01ea3
Botond Kulcsar <bkulcsar@gmx.com>
2021-10-15 11:32:01 +0300

    add Procfile and set PORT

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const morgan = require('morgan');
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
@@ -86,7 +88,7 @@ app.get(`/info`, (request, response) => {
     ${new Date()}`);
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`);
