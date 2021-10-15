1f8f121366421c8003913c63f65e97b346d8d275
Botond Kulcsar <bkulcsar@gmx.com>
2021-10-15 18:03:49 +0300

    exercises 3.15.-3.18.

--- index.js
+++ index.js
@@ -13,9 +13,20 @@ app.use(express.json());
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`));
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({
+            error: 'malformatted id'
+        })
+    }
+
+    next(error)
+}
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const { name, number } = request.body;
 
     if (!name || !number) {
@@ -26,48 +37,66 @@ app.post('/api/persons', (request, response) => {
     }
 
     const newPerson = new Person({ name, number })
-   
-    newPerson.save().then(savedPerson => {
-        response.status(201);
-        response.json(savedPerson);
-    })
+
+    newPerson.save()
+        .then(savedPerson => {
+            response.status(201);
+            response.json(savedPerson);
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons);
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(persons => {
+            response.json(persons);
+        })
+        .catch(error => next(error))
 })
 
-app.get(`/api/persons/:id`, (request, response) => {
-    // const id = Number(request.params.id);
-    // const person = persons.find(p => p.id === id);
-    // if (!person) {
-    //     return response.status(404).send(`No person with id=${id} has been found`)
-    // }
-    Person.findById(request.params.id).then(person => {
-        response.json(person);
-    })
-    
+app.get(`/api/persons/:id`, (request, response, next) => {
+
+    Person.findById(request.params.id)
+        .then(person => {
+            response.json(person);
+        })
+        .catch(error => next(error))
+
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(p => p.id === id);
-    if (!person) {
-        return response.status(404).send(`No person with id=${id} has been found`)
-    }
+app.put(`/api/persons/:id`, (request, response, next) => {
+    const { name, number } = request.body;
+    const personToUpdate = { name, number };
 
-    persons = persons.filter(p => p.id !== id);
-    response.status(204).end();
+    Person.findByIdAndUpdate(request.params.id, personToUpdate, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson);
+        })
+        .catch(error => next(error))
 })
 
-app.get(`/info`, (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people
-    ${new Date()}`);
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error))
+})
+
+app.get(`/info`, (request, response, next) => {
+    Person.find({})
+        .then(persons => {
+            response.send(`Phonebook has info for ${persons.length} people ${new Date()}`);
+        })
+        .catch(error => next(error))
+
 })
 
 const PORT = process.env.PORT;
+
+
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`);
