7e13d2a8821d658260a4345af06b537ebd9f5cba
Botond Kulcsar <bkulcsar@gmx.com>
2021-10-15 15:20:07 +0300

    exercise 3.12 mongo.js

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+    console.log(`Please provide the password as an argument: node mongo.js <password>`);
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url =
+    `mongodb+srv://kecs:${password}@cluster0.twaol.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 5) {
+    const newPerson = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    });
+
+    newPerson
+        .save()
+        .then(savedPerson => {
+            console.log(`added ${savedPerson.name} number ${savedPerson.number} to phonebook`);
+            mongoose.connection.close()
+        })
+}
+
+if (process.argv.length === 3) {
+    Person
+        .find({})
+        .then(persons => {
+            console.log('phonebook:');
+            persons.forEach(p => {
+                console.log(`${p.name} ${p.number}`);
+            });
+            mongoose.connection.close();
+        })
+}
