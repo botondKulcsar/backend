ea6fc85890380ede301f67a46e4bb6642b67be15
Botond Kulcsar <bkulcsar@gmx.com>
2021-10-15 20:46:24 +0300

    exercise 3.22

--- index.js
+++ index.js
@@ -1,107 +1,107 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const Person = require('./models/person');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Person = require('./models/person')
 
-const morgan = require('morgan');
+const morgan = require('morgan')
 
-app.use(cors());
-app.use(express.static('build'));
-app.use(express.json());
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
-app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({
-            error: 'malformatted id'
-        })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).send({
-            error: error.message
-        })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({
+      error: error.message
+    })
+  }
 
-    next(error)
+  next(error)
 }
 
 
 app.post('/api/persons', (request, response, next) => {
-    const { name, number } = request.body;
+  const { name, number } = request.body
 
-    if (!name || !number) {
-        response.status(400);
-        return response.json({
-            error: `Request body is missing required fields`
-        })
-    }
+  if (!name || !number) {
+    response.status(400)
+    return response.json({
+      error: 'Request body is missing required fields'
+    })
+  }
 
-    const newPerson = new Person({ name, number })
+  const newPerson = new Person({ name, number })
 
-    newPerson.save()
-        .then(savedPerson => {
-            response.status(201);
-            response.json(savedPerson);
-        })
-        .catch(error => next(error))
+  newPerson.save()
+    .then(savedPerson => {
+      response.status(201)
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({})
-        .then(persons => {
-            response.json(persons);
-        })
-        .catch(error => next(error))
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
-app.get(`/api/persons/:id`, (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
 
-    Person.findById(request.params.id)
-        .then(person => {
-            response.json(person);
-        })
-        .catch(error => next(error))
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => next(error))
 
 })
 
-app.put(`/api/persons/:id`, (request, response, next) => {
-    const { name, number } = request.body;
-    const personToUpdate = { name, number };
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+  const personToUpdate = { name, number }
 
-    Person.findByIdAndUpdate(request.params.id, personToUpdate, { new: true, runValidators: true })
-        .then(updatedPerson => {
-            response.json(updatedPerson);
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, personToUpdate, { new: true, runValidators: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end();
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.get(`/info`, (request, response, next) => {
-    Person.find({})
-        .then(persons => {
-            response.send(`Phonebook has info for ${persons.length} people ${new Date()}`);
-        })
-        .catch(error => next(error))
+app.get('/info', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.send(`Phonebook has info for ${persons.length} people ${new Date()}`)
+    })
+    .catch(error => next(error))
 
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`);
+  console.log(`server is listening on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,41 +1,41 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log('connecting to ', url);
+console.log('connecting to ', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB: ', error.message);
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        uniqueCaseInsensitive: true,
-        minlength: 3
-    },
-    number: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 8
-    }
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    uniqueCaseInsensitive: true,
+    minlength: 3
+  },
+  number: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 8
+  }
 })
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
